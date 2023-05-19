f147a88af8d71cf659f4841386bf20da7fa87c05
Eugene Fedchuk <fed4chuck@gmail.com>
2023-05-19 20:50:48 +0300

    Added logging

--- garden_of_glass/mana_unloader.lua
+++ garden_of_glass/mana_unloader.lua
@@ -1,7 +1,16 @@
+local arg = ...
+local log
+if arg == "-v" then
+  log = true
+end
+
 local pumping = false
 while true do
   rs.setOutput("top", not pumping)
   os.pullEvent("redstone")
+  if log then
+    print(rs.getAnalogInput("back"))
+  end
   if pumping then
     if rs.getAnalogInput("back") >= 13 then
       pumping = false
